01d14d2efb40266900fd1bc0c0c719883d67ae50
Elmer <admin@aprode.org>
2016-11-03 20:57:09 -0500

    Added comments table, controller and actions

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ActiveRecord::Base
 	belongs_to :user
+	has_many :comments
 	validates :title, presence: true, length: {minimum: 3, maximum: 20}
 	validates :description, presence: true, length: {minimum: 10, maximum: 500}
 end
