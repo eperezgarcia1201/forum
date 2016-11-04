968edb7db4cdddbaf3c22571f70b6c49a68c6968
Elmer <admin@aprode.org>
2016-11-03 19:40:26 -0500

    Added function to display data in the index and the show Actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.all.order('created_at DESC')
   end
 
   def show
+    @message = Message.find(params[:id])
   end
 
   def new
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(messages_params)
+    if @message.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+
   end
 
   def edit
@@ -18,5 +28,11 @@ class MessagesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private 
+
+  def messages_params
+    params.require(:message).permit(:title, :description)
   end
 end
