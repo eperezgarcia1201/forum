9563573b553827aa81219452daa83581a3aa60f2
Elmer <admin@aprode.org>
2016-11-04 12:50:49 -0500

    Added comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 	def create
 		@message = Message.find(params[:message_id])
-		@message = @message.comments.create(comment_params)
+		@comment = @message.comments.create(comment_params)
 		@comment.user_id = current_user.id
 
 		if @comment.save
@@ -12,6 +12,6 @@ class CommentsController < ApplicationController
 	end
 	private
 	def comment_params
-		params.require(:comment).permit(:content)
+		params.require(:comment).permit(:content, :user_id)
 	end
 end
